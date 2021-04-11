4e14e6685c3ede4a0994b5d6d1c7b34bfca67aa1
Sailesh M <43719932+Saileshmurali@users.noreply.github.com>
2021-04-11 13:38:26 +0530

    Update script3.py

--- script3.py
+++ script3.py
@@ -1,21 +1,6 @@
 from Adafruit_IO import Client, Feed, RequestError
-"""
-#ADAFRUIT_IO_USERNAME = '123dragon'
-#ADAFRUIT_IO_KEY = 'aio_BGPJ15el87aqpfdYDQ7PntUEJWdh'
-aio = Client(ADAFRUIT_IO_USERNAME, ADAFRUIT_IO_KEY)
-
-#test=aio.feeds('test')
-v=aio.feeds('voltage')
-i=aio.feeds('current')
-aio.send(v.key,421.234)
-aio.send(i.key,4.3452)
-#aio.send(test.key,42)
-
-#data = aio.receive('Voltage')
-#print('Received value: {0}'.format(data.value))
-"""
-ADAFRUIT_IO_KEY = 'aio_SKcv478WCyQ9FdIz51YuwG4YOKOV'
-ADAFRUIT_IO_USERNAME = 'ssudhirsai'
+ADAFRUIT_IO_KEY = 'type key here'
+ADAFRUIT_IO_USERNAME = 'type user name here'
 aio = Client(ADAFRUIT_IO_USERNAME, ADAFRUIT_IO_KEY)
 try:
     voltage = aio.feeds('voltage')
